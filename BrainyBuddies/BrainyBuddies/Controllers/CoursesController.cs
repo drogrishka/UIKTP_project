3f3243f0b73fed899a2a1c2683256bccea0f3237
Filip Kramarski <80000955+filipkramarski@users.noreply.github.com>
2023-05-05 18:10:37 +0200

    implemented CRUD functionality

--- BrainyBuddies/BrainyBuddies/Controllers/CoursesController.cs
+++ BrainyBuddies/BrainyBuddies/Controllers/CoursesController.cs
@@ -25,5 +25,75 @@ namespace BrainyBuddies.Controllers
             List<Course> list = _courseContext.Courses.ToList();
             return StatusCode(StatusCodes.Status200OK, list);
         }
+
+        [HttpGet]
+        [Route("GetCourse/{id}")]
+        public IActionResult GetCourseById(int id)
+        {
+            var course = _courseContext.Courses.FirstOrDefault(c => c.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return StatusCode(StatusCodes.Status200OK, course);
+        }
+
+        [HttpPost]
+        [Route("AddCourse")]
+        public IActionResult AddCourse([FromBody] Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _courseContext.Courses.Add(course);
+            _courseContext.SaveChanges();
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpDelete]
+        [Route("DeleteCourse/{id}")]
+        public IActionResult DeleteCourse(int id)
+        {
+            var course = _courseContext.Courses.FirstOrDefault(c => c.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            _courseContext.Courses.Remove(course);
+            _courseContext.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("EditCourse/{id}")]
+        public IActionResult EditCourse(int id, [FromBody] Course course)
+        {
+            if (id != course.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingCourse = _courseContext.Courses.FirstOrDefault(c => c.Id == id);
+            if (existingCourse == null)
+            {
+                return NotFound();
+            }
+
+            existingCourse.ImgURL = course.ImgURL;
+            existingCourse.Ime = course.Ime;
+            existingCourse.Pisatel = course.Pisatel;
+            existingCourse.Predmet = course.Predmet;
+            existingCourse.Opis = course.Opis;
+
+            _courseContext.SaveChanges();
+
+            return Ok();
+        }
     }
 }
